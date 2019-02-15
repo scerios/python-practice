71040756167c260373446e2f6ead5c6e657ed9ed
Takács Róbert <89.t.robert@gmail.com>
2019-02-15 10:36:23 +0100

    feat(tuples): tuple and list comparison

---
+++ tuples.py
@@ -0,0 +1,11 @@
+import sys
+import timeit
+
+# List -add, -remove, -change
+primes = [2, 3, 5, 7, 11, 13, 17]
+
+# Tuple is FINAL
+squaresOfTwo = (2, 4, 8, 16, 32, 64)
+
+print("Primes:", len(primes))
+print("Squares:", len(squaresOfTwo))
