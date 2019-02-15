6cd64e8337d6cb22f2cec0833c77db5cef5fd011
Takács Róbert <89.t.robert@gmail.com>
2019-02-15 10:26:26 +0100

    feat(rng): import random and example of .random()

---
+++ rng.py
@@ -0,0 +1,7 @@
+import random
+
+# Display 10 random numbers from interval [0, 1). ")" show it will never return 1
+
+for i in range(10):
+    # random() ca be used to build customized random number generators
+    print(random.random())
