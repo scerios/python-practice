519afdd9a09a3d512ca2c61a0104b25970599290
Takács Róbert <89.t.robert@gmail.com>
2019-02-15 10:35:14 +0100

    feat(if_then_else): if else

--- if_then_else.py
+++ if_then_else.py
@@ -1,3 +1,23 @@
 if len(input("Please enter a test string:")) < 6:
     print("Your string is too short.")
     print("Please enter a string with at least 6 characters!")
+
+question = "How old are you?"
+if len(input(question)) < 2:
+    print("You must be at least 10 years old to fuck.")
+else:
+    print("You can fuck")
+evenOrOdd = "Test a number if it's even or odd! Enter your number: "
+number = int(input(evenOrOdd))
+
+if number % 2 == 0:
+    print("Your number is even")
+else:
+    print("Your number is odd")
+
+a = "The lenght of side a = "
+aSide = int(input(a))
+b = "The lenght of side b = "
+bSide = int(input(b))
+c = "The lenght of side c = "
+cSide = int(input(c))
