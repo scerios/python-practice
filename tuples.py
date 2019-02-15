4090aeeed5eb19e7e5caa4bd19ff963e45abec50
Takács Róbert <89.t.robert@gmail.com>
2019-02-15 10:37:11 +0100

    feat(tuples): tuple and list comparison about memory usage

--- tuples.py
+++ tuples.py
@@ -9,3 +9,16 @@ squaresOfTwo = (2, 4, 8, 16, 32, 64)
 
 print("Primes:", len(primes))
 print("Squares:", len(squaresOfTwo))
+
+for i in primes:
+    print("Prime:", i)
+
+for i in squaresOfTwo:
+    print("Square:", i)
+
+list = [2, "kys", True, 1.5]
+tuple = (2, "kys", True, 1.5)
+
+# How much memory does it use
+print("List:", sys.getsizeof(list))
+print("List:", sys.getsizeof(tuple))
