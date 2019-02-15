cead59a0e82ad95fd1dd5c9158fe6cbd3161e008
Takács Róbert <89.t.robert@gmail.com>
2019-02-15 11:13:20 +0100

    feat(csv_module): file reading and print all lines

---
+++ csv_module.py
@@ -0,0 +1,11 @@
+# [C]omma [S]eparated [V]alue
+#
+# Title, Release year, Director
+# The Godfather, 1972, Francis Ford Coppola
+# The Shawshank Redemption, 1994, Frank Darabont
+# Citizen Kane, 1941, Orson Welles
+
+path = "C:\\Users\\Csirke\\greenfox\\Test Automation\\python-practice\\google_stock_data.csv"
+file = open(path)
+for line in file:
+    print(line)
