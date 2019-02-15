3163471f2addfa36238af6a75f612613ebf5dc5d
Takács Róbert <89.t.robert@gmail.com>
2019-02-15 10:27:47 +0100

    feat(rng): scaling and shifting

--- rng.py
+++ rng.py
@@ -5,3 +5,17 @@ import random
 for i in range(10):
     # random() ca be used to build customized random number generators
     print(random.random())
+
+
+# Generate random numbert from interval [3, 7)
+# Call random():                        [0, 1)
+# Scale number(multiply by 4):          [0, 4)
+# Shift number(add 3):                  [3, 7)
+
+def rngNum():
+    # random, scale, shift, return
+    return 4 * random.random() + 3
+
+
+for i in range(10):
+    print(rngNum())
