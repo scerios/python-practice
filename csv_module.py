04593139ca2c898e1b13c00177c8613ca9bbbea2
Takács Róbert <89.t.robert@gmail.com>
2019-02-15 11:40:00 +0100

    feat(csv_module): separating data as header and information

--- csv_module.py
+++ csv_module.py
@@ -1,3 +1,4 @@
+import csv
 # [C]omma [S]eparated [V]alue
 #
 # Title, Release year, Director
@@ -7,19 +8,30 @@
 
 path = "C:\\Users\\Csirke\\greenfox\\Test Automation\\python-practice\\google_stock_data.csv"
 file = open(path)
-for line in file:
-    print(line)
+# for line in file:
+#     print(line)
 
 # Saving data in a list as lines
 lines = [line for line in open(path)]
-print(lines[0])
+# print(lines[0])
 
 # Remove any leading or trailing whitespace
-print(lines[0].strip())
+# print(lines[0].strip())
 
 # Division to smaller pieces
-print(lines[0].strip().split(','))
+# print(lines[0].strip().split(','))
 
 dataset = [line.strip().split(',') for line in open(path)]
-print(dataset[0])
-print(dataset[1])
+# print(dataset[0])
+# print(dataset[1])
+
+path_csv = "C:\\Users\\Csirke\\greenfox\\Test Automation\\python-practice\\google_stock_data.csv"
+file = open(path_csv, newline='')
+reader = csv.reader(file)
+
+# First line is a header
+header = next(reader)
+# Read the remaining data
+data = [row for row in reader]
+print(header)
+print(data[0])
